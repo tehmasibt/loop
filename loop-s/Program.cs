6f25d6863c51874c8f5bc48548642ea36e7a588e
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-15 16:29:37 +0400

    L

---
+++ loop-s/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, World!");
+
+
+#region task-1
+//int skafen = 2;
+//int skafuz = 3;
+//int qapien = 9;
+//int qapiuz = 2;
+//if (skafen*skafuz<qapien*qapiuz)
+//{
+//    Console.WriteLine("skaf qapidan kecir");
+//}
+//else
+//{
+//    Console.WriteLine("skaf qapidan kecmir");
+//}
+#endregion
+#region task-3
+//int a = 387443;
+//int result = 0;
+//while (a>0)
+//{
+//    int b = a % 10;
+//    a /= 10;
+//    result++;
+
+//}
+//Console.WriteLine(result);
+#endregion
